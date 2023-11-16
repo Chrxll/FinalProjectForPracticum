92e1976b2c12c2033d9ea11651558c3293c6acf4
Chrxll <120812022+Chrxll@users.noreply.github.com>
2023-11-16 13:30:22 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Богомолов Игорь, 10-я когорта — Финальный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://7262ccd9-ee92-4359-9e9f-1fc84ab4b77e.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
